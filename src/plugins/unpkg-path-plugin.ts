90b29517b7382eba1f65b88e7fa8fa9f9ddb9d56
Vlad <ilin96inbox@gmail.com>
2021-05-05 21:15:41 +0900

    resolve nested paths

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -14,7 +14,10 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes("./") || args.path.includes("../")) {
           return {
             namespace: "a",
-            path: new URL(args.path, args.importer + "/").href,
+            path: new URL(
+              args.path,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href,
           };
         }
 
@@ -22,12 +25,6 @@ export const unpkgPathPlugin = () => {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`,
         };
-        //        else if (args.path === "tiny-test-pkg") {
-        //          return {
-        //            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
-        //            namespace: "a",
-        //          };
-        //        }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -37,14 +34,19 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from 'medium-test-pkg';
+              import message from 'nested-test-pkg';
               console.log(message);
             `,
           };
         }
 
-        const { data } = await axios.get(args.path);
-        return { loader: "jsx", contents: data };
+        const { data, request } = await axios.get(args.path);
+
+        return {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
       });
     },
   };

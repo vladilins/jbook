aab78160c72eefea682cb8684223e20250583989
Vlad <ilin96inbox@gmail.com>
2021-05-05 21:02:13 +0900

    add url constructor

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,11 +10,18 @@ export const unpkgPathPlugin = () => {
         if (args.path === "index.js") {
           return { path: args.path, namespace: "a" };
         }
-        
-        return {
-          namespace: 'a',
-          path: `https://unpkg.com/${args.path}`
+
+        if (args.path.includes("./") || args.path.includes("../")) {
+          return {
+            namespace: "a",
+            path: new URL(args.path, args.importer + "/").href,
+          };
         }
+
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
         //        else if (args.path === "tiny-test-pkg") {
         //          return {
         //            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
@@ -30,7 +37,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from 'tiny-test-pkg';
+              import message from 'medium-test-pkg';
               console.log(message);
             `,
           };

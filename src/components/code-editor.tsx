4e439f4149e633270961f90d8f329f5155800e34
Vlad <ilin96inbox@gmail.com>
2021-05-10 19:56:08 +0900

    editor config

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -5,6 +5,13 @@ const CodeEditor = () => {
     <MonacoEditor
       options={{
         wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
       }}
       theme="dark"
       language="javascript"

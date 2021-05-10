76faddb91218a524ee2a158e292bb4d1530725b0
Vlad <ilin96inbox@gmail.com>
2021-05-10 19:58:52 +0900

    set initial value

--- src/index.tsx
+++ src/index.tsx
@@ -65,7 +65,7 @@ const App = () => {
 
   return (
     <div>
-      <CodeEditor></CodeEditor>
+      <CodeEditor initialValue="const a = 1;"></CodeEditor>
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}

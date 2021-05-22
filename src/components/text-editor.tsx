c1dbfc3cc690bd6bb8c93088f0dd8e360113dec7
Vlad <ilin96inbox@gmail.com>
2021-05-22 21:41:08 +0900

    toggele editing mode

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MDEditor from "@uiw/react-md-editor";
 
 const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = (e: any) => {
+      setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div>
+        <MDEditor />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <MDEditor />
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={"# Header"} />
     </div>
   );
 };

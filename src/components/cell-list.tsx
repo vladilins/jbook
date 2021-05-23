c1b9e4f6e6f744f4f65eda5ffb93b3d079d88726
Vlad <ilin96inbox@gmail.com>
2021-05-23 19:29:09 +0900

    add divider

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import AddCell from "./add-cell";
 import CellListItem from "./cell-list-item";
@@ -9,10 +9,10 @@ const CellList: React.FC = () => {
   });
 
   const renderedCells = cells.map((cell) => (
-    <>
+    <Fragment key={cell.id}>
       <AddCell nextCellId={cell.id} />
-      <CellListItem key={cell.id} cell={cell} />
-    </>
+      <CellListItem cell={cell} />
+    </Fragment>
   ));
 
   return (

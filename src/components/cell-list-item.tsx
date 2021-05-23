bbeac4a4b56a59908a04203eb58b9ad2792738b6
Vlad <ilin96inbox@gmail.com>
2021-05-23 17:49:00 +0900

    action creator helper

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { Cell } from "../state";
 import CodeCell from "./code-cell";
 import TextEditor from "./text-editor";
@@ -8,6 +9,8 @@ interface CellListItemProps {
 }
 
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+  const dispatch = useDispatch()
+  
   let child: JSX.Element;
   if (cell.type === "code") {
     child = <CodeCell />;

93de6806d2c4624d352b991c47366ff2eef0a89c
Vlad <ilin96inbox@gmail.com>
2021-05-24 21:47:35 +0900

    fix background

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import './code-cell.css'
+import "./code-cell.css";
 import { useEffect } from "react";
 import CodeEditor from "./code-editor";
 import Resizable from "./resizable";
@@ -46,15 +46,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {!bundle || bundle.loading ? (
-          <div className="progress-cover">
-            <progress className="progress is-small is-primary" max="100">
-              Loading
-            </progress>
-          </div>
-        ) : (
-          <Preview code={bundle?.code} err={bundle?.err} />
-        )}
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle?.code} err={bundle?.err} />
+          )}
+        </div>
       </div>
     </Resizable>
   );

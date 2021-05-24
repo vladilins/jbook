26a6dd3203b21ae2d7f96d9d834343ee1f45610b
Vlad <ilin96inbox@gmail.com>
2021-05-24 21:19:48 +0900

    adjust reducer state

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import CodeEditor from "./code-editor";
 import Resizable from "./resizable";
 import { Cell } from "../state";
+import Preview from "./preview";
 import { useActions } from "../hooks/use-actions";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 
@@ -12,7 +13,6 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
-  console.log(bundle);
 
   useEffect(() => {
     const timer = setTimeout(async () => {
@@ -38,7 +38,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {/* <Preview code={code} err={err!} /> */}
+        {bundle && <Preview code={bundle?.code} err={bundle?.err} />}
       </div>
     </Resizable>
   );

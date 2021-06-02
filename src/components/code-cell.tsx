6c15249700b337990ee755490f09f4572dcc72db
Vlad <ilin96inbox@gmail.com>
2021-06-02 20:43:44 +0900

    execute cumulitive code

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -30,23 +30,21 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     return cumulativeCode;
   });
 
-  console.log(cumulativeCode);
-
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join("\n"));
       return;
     }
 
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join("\n"));
     }, 1000);
     return () => {
       clearTimeout(timer);
     };
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.id, cell.content, createBundle]);
+  }, [cumulativeCode.join("\n"), cell.content, createBundle]);
 
   return (
     <Resizable direction="vertical">

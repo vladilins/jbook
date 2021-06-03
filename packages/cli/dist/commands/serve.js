86bfe2b65ded6ec8835847ec544a4aa91a93deea
Vlad <ilin96inbox@gmail.com>
2021-06-03 21:22:01 +0900

    communicate from cli to api

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -2,11 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
 var commander_1 = require("commander");
+var local_api_1 = require("local-api");
 exports.serveCommand = new commander_1.Command()
     .command("serve [filename]")
     .description("Open a file for editing")
     .option("-p, --port <number>", "port to run server on", "4005")
     .action(function (filename, options) {
     if (filename === void 0) { filename = "notebook.js"; }
-    console.log(filename, options);
+    local_api_1.serve(+options.port, filename, "/");
 });

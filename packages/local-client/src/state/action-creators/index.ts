57811b7ea21f57645545b1ce502ae089c733b1ea
Vlad <ilin96inbox@gmail.com>
2021-06-05 21:27:14 +0900

    fetch cells action creator

--- packages/local-client/src/state/action-creators/index.ts
+++ packages/local-client/src/state/action-creators/index.ts
@@ -1,4 +1,5 @@
 import { Dispatch } from "react";
+import axios from "axios";
 import bundle from "../../bundler";
 import { ActionType } from "../action-types";
 import {
@@ -9,7 +10,7 @@ import {
   MoveCellAction,
   UpdateCellAction,
 } from "../actions";
-import { CellTypes } from "../cell";
+import { CellTypes, Cell } from "../cell";
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -69,5 +70,27 @@ export const createBundle = (cellId: string, input: string) => {
         bundle: result,
       },
     });
+  };
+};
+
+export const fetchCells = () => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.FETCH_CELLS,
+    });
+
+    try {
+      const { data }: { data: Cell[] } = await axios.get("/cells");
+
+      dispatch({
+        type: ActionType.FETCH_CELLS_COMPLETE,
+        payload: data,
+      });
+    } catch (err) {
+      dispatch({
+        type: ActionType.FETCH_CELLS_ERROR,
+        payload: err.message,
+      });
+    }
   };
 };
